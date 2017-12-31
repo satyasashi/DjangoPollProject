3f58f5d7466221a0eb226ed0a6cb21d324a6e974
venkata krishna <sashirubik@gmail.com>
2017-12-31 18:22:12 +0530

    Added test

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -10,8 +10,26 @@ class Question(models.Model):
 	def __str__(self):
 		return self.question_text
 
+	# Checks if post published >= yesterday
 	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+		#return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+		# As above time calculation is a Bug for future timing.
+		# i.e. was_published_recently() is also being true for future published
+		# which is not possible.
+
+		'''Here is how it works:
+			- Works on Published Date, Yesterday, Today
+			- which makes sure Publishment was RECENT (only 1day and today) not in 'Future'
+			- example: 					
+					> yesterday = 30th
+					> publishedOn = 25th
+					> today(now) = 31st
+					
+					> 30 <= 25 <= 31 ---> 30 <= 25 is False, 
+		'''
+		now = timezone.now() # Present time
+		yesterday = timezone.now() - datetime.timedelta(days=1) # Yesterday's time
+		return yesterday <= self.pub_date <= now
 
 class Choice(models.Model):
 	question 	=	models.ForeignKey(Question, on_delete=models.CASCADE)
--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,20 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
+from .models import Question # importing the models we want to test
+
 # Create your tests here.
+class QuestionModelTest(TestCase):
+	def test_was_published_recently_with_future_question(self):
+		"""
+			was_published_recently() will return False for the questions
+			whose pub_date was in future
+		"""
+
+		# Future time
+		time = timezone.now() + datetime.timedelta(days=30)
+		# Future question - Created a question 30 days in future
+		future_question = Question(pub_date=time)
+		self.assertIs(future_question.was_published_recently(), False)
