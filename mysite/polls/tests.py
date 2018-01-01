137f055ba8023883a63d1482895e86367582a236
sashikanth <sashirubik@gmail.com>
2018-01-02 00:31:26 +0530

    Added test_future_question to QuestionIndexViewTests

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -11,6 +11,7 @@ from .models import Question # importing the models we want to test
 
 # Create your tests here.
 class QuestionModelTest(TestCase):
+
 	def test_was_published_recently_with_future_question(self):
 		"""
 			was_published_recently() will return False for the questions
@@ -19,7 +20,7 @@ class QuestionModelTest(TestCase):
 
 		# Future time
 		time = timezone.now() + datetime.timedelta(days=30)
-		# Future question - Created a question 30 days in future
+		# Future question -> Created a question 30 days in future
 		future_question = Question(pub_date=time)
 		self.assertIs(future_question.was_published_recently(), False)
 
@@ -48,6 +49,10 @@ class QuestionModelTest(TestCase):
 			given number of `days` offset to now (negative for questions published
 			in the past, Positive for questions that have yet to be published).
 		"""
+		time = timezone.now() + datetime.timedelta(days=days)
+		return Question.objects.create(question_text=question_text, pub_date=time)
+
+
 
 class QuestionIndexViewTests(TestCase):
 	def test_no_questions(self):
@@ -62,4 +67,25 @@ class QuestionIndexViewTests(TestCase):
 
 	def test_past_question(self):
 		"""
+		Questions with a pub_date  in the past are displayed on the 
+		Index page
 		"""
+		create_question(question_text="Past question.", days=-30)
+		response = self.client.get(reverse('polls:index'))
+
+		"""As we used our custom create function to create
+		a Question -> Past question, we check if context of 
+		'latest_question_list' if it has 'Past question'
+        """
+        self.assertQuerysetEqual(
+        	response.context['latest_question_list'],['<Question: Past question.>']
+        	)
+    def test_future_question(self):
+    	"""
+		Questions with a pub_date in the future aren't displayed on the index page
+    	"""
+    	create_question(question_text="Future question.", days=30)
+    	response = self.client.get(reverse('polls:index'))
+    	self.assertContains(response, "No polls are available.")
+
+    	self.assertQuerysetEqual(response.context['latest_question_list'], [])
